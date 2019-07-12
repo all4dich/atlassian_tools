60a55e7e7d53ab4dbb5cc3768f36a65c29971006
Sunjoo Park <all4dich@gmail.com>
2019-07-12 16:48:42 +0900

    Use a variable to set package's root directory

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, find_packages
 
+python_src_root = "src/main/python"
 setup(
     name='atlassian_tools',
     version='0.1',
@@ -7,6 +8,6 @@ setup(
     description='Custom tools for Atlassian Product',
     author='Sunjoo Park',
     author_email='all4dich@gmail.com',
-    package_dir={'': 'src/main/python/'},
-    packages=find_packages(where='src/main/python/')
+    package_dir={'': python_src_root},
+    packages=find_packages(where=python_src_root)
 )

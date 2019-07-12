7e0bf8afea8043576c6890208f390135c51eedfb
Sunjoo Park <all4dich@gmail.com>
2019-07-12 16:23:09 +0900

    Move all python codes under 'src/main/python'

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 from setuptools import setup, find_packages
 
-#packages=['lge'],
-#package_dir={'': 'src/main/python'}
 setup(
-    name='confluence',
+    name='atlassian_tools',
     version='0.1',
     zip_safe=False,
     description='Custom tools for Atlassian Product',
     author='Sunjoo Park',
     author_email='all4dich@gmail.com',
-    package_dir={'': 'src'},
-    packages=find_packages(where='src')
+    package_dir={'': 'src/main/python/'},
+    packages=find_packages(where='src/main/python/')
 )

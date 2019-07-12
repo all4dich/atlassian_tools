51cd92fa30baba65be5f485a06e80bd9954c3849
Sunjoo Park <all4dich@gmail.com>
2019-07-12 15:52:05 +0900

    Second sample

--- src/atlassian/main.py
+++ src/atlassian/main.py
@@ -1,7 +1,10 @@
-from src import hello
 import sys
+from . import hello
 
-if __name__ == "__main__":
+def main():
     hello.world()
     for each_path in sys.path:
         print(each_path)
+
+if __name__ == "__main__":
+    main()
